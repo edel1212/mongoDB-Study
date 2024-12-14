ac447d6e78db1d9bcd0758e263ea699e87435bb7
yoo <edel1212@naver.com>
2024-12-14 18:04:04 +0900

    project init

---
+++ use-spring/single-mongo/src/main/java/com/yoo/single_mongo/SingleMongoApplication.java
@@ -0,0 +1,13 @@
+package com.yoo.single_mongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SingleMongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SingleMongoApplication.class, args);
+	}
+
+}
---
+++ use-spring/single-mongo/src/test/java/com/yoo/single_mongo/SingleMongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yoo.single_mongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SingleMongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
