9ae9f7aed2178a99e496d6f83f30202ce53fdc9d
yoo <edel1212@naver.com>
2024-12-14 21:40:48 +0900

    지정 필드 유니크 값 설정 추가 - application 추가 설정 필요

--- use-spring/single-mongo/src/main/java/com/yoo/single_mongo/entity/Member.java
+++ use-spring/single-mongo/src/main/java/com/yoo/single_mongo/entity/Member.java
@@ -2,6 +2,7 @@ package com.yoo.single_mongo.entity;
 
 import lombok.*;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @AllArgsConstructor
@@ -14,6 +15,11 @@ public class Member {
     // MongoDB의 DefaultId
     @Id
     private String id;
+
+    // 고유값 성정
+    @Indexed(unique = true)
     private String accountId;
+
+    @Setter
     private int age;
 }
