8d523b6bce0cc8a36119b96f6ec39655a16b70c5
yoo <edel1212@naver.com>
2024-12-14 20:41:24 +0900

    Entity 추가

---
+++ use-spring/single-mongo/src/main/java/com/yoo/single_mongo/entity/Member.java
@@ -0,0 +1,19 @@
+package com.yoo.single_mongo.entity;
+
+import lombok.*;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Builder
+@ToString
+@Document("member")
+public class Member {
+    // MongoDB의 DefaultId
+    @Id
+    private String id;
+    private String accountId;
+    private int age;
+}
