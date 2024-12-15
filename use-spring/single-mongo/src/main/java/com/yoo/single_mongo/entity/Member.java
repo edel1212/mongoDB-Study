8265d486e6f1156f93804d21d6d29fd574bcbff3
yoo <edel1212@naver.com>
2024-12-15 15:08:03 +0900

    Member Entity 내 범위 조회 테스트를 위한 joinedDate 필드 추가

--- use-spring/single-mongo/src/main/java/com/yoo/single_mongo/entity/Member.java
+++ use-spring/single-mongo/src/main/java/com/yoo/single_mongo/entity/Member.java
@@ -5,6 +5,8 @@ import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import java.time.LocalDateTime;
+
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
@@ -22,4 +24,7 @@ public class Member {
 
     @Setter
     private int age;
+
+    @Setter
+    private LocalDateTime joinedDate;
 }
